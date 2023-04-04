c3af39913ee98a6b56c5c25f777594553220a0c0
Denis Kulik <kulikdenis1996x@gmail.com>
2023-04-04 15:05:33 +0700

    add the ability to filter tasks

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type PropsType = {
     title: string
     tasks: TaskType[]
@@ -11,33 +13,58 @@ type TaskType = {
 }
 
 const Todolist = ({ title, tasks, deleteTask }: PropsType) => {
-    const taskItem = tasks.map(task => {
-        return (
-            <li key={ task.id }>
-                <input type="checkbox" checked={ task.isDone } />
-                <span>{ task.title }</span>
-                <button onClick={ () => deleteTask(task.id) }>X</button>
-            </li>
-        );
-    });
+        const [ filterType, setFilterType ] = useState('all');
 
-    return (
-        <div>
-            <h3>{ title }</h3>
+        const filterTasks = (valueBtn: string) => setFilterType(valueBtn);
+
+        let filteredTasks = tasks;
+
+        switch (filterType) {
+            case 'all':
+                filteredTasks = tasks;
+                break;
+            case 'active':
+                filteredTasks = tasks.filter(task => !task.isDone);
+                break;
+            case 'completed':
+                filteredTasks = tasks.filter(task => task.isDone);
+                break;
+        }
+
+        const taskItem = filteredTasks.map(task => {
+            return (
+                <li key={ task.id }>
+                    <input type="checkbox" checked={ task.isDone } />
+                    <span>{ task.title }</span>
+                    <button onClick={ () => deleteTask(task.id) }>X</button>
+                </li>
+            );
+        });
+
+        return (
             <div>
-                <input />
-                <button>+</button>
+                <h3>{ title }</h3>
+                <div>
+                    <input />
+                    <button>+</button>
+                </div>
+                <ul>
+                    { taskItem }
+                </ul>
+                <div>
+                    <button onClick={ () => filterTasks('all') }>
+                        All
+                    </button>
+                    <button onClick={ () => filterTasks('active') }>
+                        Active
+                    </button>
+                    <button onClick={ () => filterTasks('completed') }>
+                        Completed
+                    </button>
+                </div>
             </div>
-            <ul>
-                { taskItem }
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+;
 
 export default Todolist;
