c891c990e52738aa0f888a55990a7133f1478fb9
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-01 19:15:16 +0700

    add thunk types

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,12 +1,25 @@
-import { combineReducers, legacy_createStore as createStore } from 'redux';
+import {
+    AnyAction, applyMiddleware, combineReducers,
+    legacy_createStore as createStore
+} from 'redux';
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import tasksReducer from './tasksReducer';
 import todolistsReducer from './todolistsReducer';
+import { useDispatch } from 'react-redux';
 
 export const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppDispatchType = ThunkDispatch<AppRootStateType, unknown, AnyAction>;
+export const useAppDispatch = () => useDispatch<AppDispatchType>();
+export type AppThunkType<ReturnType = void> = ThunkAction<
+    ReturnType,
+    AppRootStateType,
+    unknown,
+    AnyAction
+>
