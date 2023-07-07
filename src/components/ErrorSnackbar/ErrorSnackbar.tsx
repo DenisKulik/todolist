b95d4c04a1e1474871c71374bb981b6157c5a383
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-07 18:57:23 +0700

    fixup! add ErrorSnackbar component

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,27 +1,29 @@
-import React, { useState } from 'react'
-import Snackbar from '@mui/material/Snackbar'
-import MuiAlert, { AlertProps } from '@mui/material/Alert'
+import React, { useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
-})
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export function ErrorSnackbar() {
-    const [open, setOpen] = useState(true)
+    const [ open, setOpen ] = useState(true);
 
-    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    const handleClose = (event?: React.SyntheticEvent | Event,
+        reason?: string) => {
         if (reason === 'clickaway') {
-            return
+            return;
         }
-        setOpen(false)
-    }
+        setOpen(false);
+    };
 
     return (
         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity='error' sx={{width: '100%'}}>
+            <Alert onClose={handleClose} severity="error"
+                   sx={{ width: '100%' }}>
                 Error message 😠
             </Alert>
         </Snackbar>
-    )
+    );
 }
