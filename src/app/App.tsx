edaa08735e30159f3fe81e96b3871ca02934e293
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-07 18:52:13 +0700

    add ErrorSnackbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import Container from '@mui/material/Container';
 import ButtonAppBar from '../components/ButtonAppBar/ButtonAppBar';
 import { Todolists } from '../features/Todolists/Todolists';
+import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar';
 
 const App = () => {
     return (
         <>
+            <ErrorSnackbar />
             <ButtonAppBar />
             <Container fixed>
                 <Todolists />
