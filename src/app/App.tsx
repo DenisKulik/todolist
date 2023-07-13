a933c2876ba5af2f8ec587d0c2d6eab5aa6f21fc
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-13 18:30:15 +0700

    add 404 page and wildcard route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import ButtonAppBar from '../components/ButtonAppBar/ButtonAppBar';
 import { Todolists } from '../features/Todolists/Todolists';
@@ -18,6 +18,8 @@ const App = ({ demo = false }: AppPropsType) => {
                 <Routes>
                     <Route path="/" element={<Todolists demo={demo} />} />
                     <Route path="login" element={<Login />} />
+                    <Route path="/404" element={<h1>404: PAGE NOT FOUND</h1>} />
+                    <Route path="*" element={<Navigate to="/404" />} />
                 </Routes>
             </Container>
         </>
