cf737e26860507985d9eacbd9136d38dc80f94da
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-13 18:08:45 +0700

    add Login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,9 @@
+import { Route, Routes } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import ButtonAppBar from '../components/ButtonAppBar/ButtonAppBar';
 import { Todolists } from '../features/Todolists/Todolists';
 import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar';
+import { Login } from '../features/Login/Login';
 
 type AppPropsType = {
     demo?: boolean
@@ -13,7 +15,10 @@ const App = ({ demo = false }: AppPropsType) => {
             <ErrorSnackbar />
             <ButtonAppBar />
             <Container fixed>
-                <Todolists demo={demo} />
+                <Routes>
+                    <Route path="/" element={<Todolists demo={demo} />} />
+                    <Route path="login" element={<Login />} />
+                </Routes>
             </Container>
         </>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import './index.scss';
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter basename="/todolist">
+            <App />
+        </BrowserRouter>
     </Provider>
 );
 
