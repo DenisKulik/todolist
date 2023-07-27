c7f5f9ba7c1620bb971b2d78c42062ea607fd7a3
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-27 22:14:17 +0700

    remove unused import

--- src/features/Todolists/tasksReducer.ts
+++ src/features/Todolists/tasksReducer.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import {
     ErrorType,
     ResultCode,
