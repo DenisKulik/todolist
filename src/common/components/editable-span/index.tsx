495d700b3fe2e13f9613a47cda7b3f01e9e5422e
Denis Kulik <kulikdenis1996x@gmail.com>
2023-10-06 12:02:26 +0700

    refactor: add title error handling

--- src/common/components/editable-span/index.tsx
+++ src/common/components/editable-span/index.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, memo, useState } from 'react'
+import { ChangeEvent, memo, useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 type Props = {
     title: string
@@ -9,9 +10,18 @@ type Props = {
 export const EditableSpan = memo(({ title, callback, disabled }: Props) => {
     const [isEditMode, setIsEditMode] = useState<boolean>(false)
     const [localTitle, setLocalTitle] = useState<string>(title)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        isEditMode && checkCorrectLengthTitle(localTitle.trim())
+    }, [isEditMode, localTitle])
+
+    const checkCorrectLengthTitle = (title: string) => {
+        setError(title.length >= 100 ? 'Title must be less than 100 characters' : null)
+    }
 
     const onToggleEditMode = () => {
-        if (!localTitle.trim() || disabled) return
+        if (!localTitle.trim() || disabled || error) return
         isEditMode && callback(localTitle)
         setIsEditMode(!isEditMode)
     }
@@ -21,14 +31,33 @@ export const EditableSpan = memo(({ title, callback, disabled }: Props) => {
     }
 
     return isEditMode ? (
-        <input
-            type="text"
-            autoFocus
-            onBlur={onToggleEditMode}
-            onChange={onSetLocalTitle}
-            value={localTitle}
-        />
+        <InputWrapper>
+            <StyledInput
+                type="text"
+                autoFocus
+                onBlur={onToggleEditMode}
+                onChange={onSetLocalTitle}
+                value={localTitle}
+                error={error ? error.toString() : undefined}
+            />
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+        </InputWrapper>
     ) : (
         <span onDoubleClick={onToggleEditMode}>{localTitle}</span>
     )
 })
+
+// styles
+const InputWrapper = styled.span`
+    position: relative;
+`
+const StyledInput = styled.input<{ error?: string }>`
+    outline: ${({ error }) => (error ? '1px solid indianred' : 'none')};
+`
+const ErrorMessage = styled.span`
+    position: absolute;
+    top: 20px;
+    left: 0;
+    font-size: 10px;
+    color: indianred;
+`
